98b3702f1cb85a4fcf46e0d6c239ef240e44c811
Luan Henrique <lhppaixaoo@gmail.com>
2018-06-01 09:02:47 -0300

    Segundo commit

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
 }
