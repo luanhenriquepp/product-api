b478dc6235ec51178d63e45ee77c999d50e426d7
Luan Henrique <lhppaixaoo@gmail.com>
2018-06-04 11:01:53 -0300

    Implementação do delete (produto)

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -97,6 +97,8 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
